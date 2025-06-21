beb7b17fb7ac7c1b96049313908785bfda88c8e3
Peter D <oranjepeter@hotmail.com>
2025-06-21 21:10:49 +0200

    Add files via upload

---
+++ Backup/my-color-bak/src/tabs/TabPanel.js
@@ -0,0 +1,43 @@
+import jQuery from "jquery";
+import cx from 'classnames';
+import Component from "../Component";
+
+const DEFAULT_CLASS = 'react-tabs__tab-panel';
+
+class TabPanel extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	build() {
+		var {
+			children,
+			className,
+			id,
+			selected,
+			selectedClassName,
+			tabId
+		} = this.props;
+
+		var tab = jQuery( "<div>" );
+		tab.addClass(cx(className, {
+			[selectedClassName]: selected
+		}));
+
+		tab.attr("role", "tabpanel");
+		tab.attr("id", id);
+		tab.attr("aria-labelledby", tabId);
+		tab.append(children);
+
+		return tab;
+	}
+}
+
+TabPanel.defaultProps = {
+	className: DEFAULT_CLASS,
+	selectedClassName: `selected`,
+};
+
+TabPanel.tabsRole = 'TabPanel';
+
+export default TabPanel;
